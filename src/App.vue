12ff3e00922dc8c3534def688acc01627cd2b400
fangfanchuan <salife32@163.com>
2019-12-18 00:06:04 +0800

    finish the header and the postlist
    finish the header and the postlist

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <div class="main">
+      <PostList></PostList>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header'
+  import PostList from "./components/PostList";
+
+export default {
+  name: 'App',
+  components:{
+    Header,
+    PostList
+  }
+}
+</script>
+
+<style>
+  .main{
+    width: 80%;
+    margin: 0 auto;
+  }
+</style>
